1be10d4e506ff1646ed8f49d1137722a3be782d2
matheus <matheusrickbatista@aluno.ufsj.edu.br>
2024-04-05 19:11:20 -0300

    logar

---
+++ src/scripts/script.js
@@ -0,0 +1,22 @@
+const loginBtn = document.getElementById('btn-login');
+const cadastroBtn = document.getElementById('btn-cadastro');
+const loginForm = document.getElementById('loginForm');
+const cadastroForm = document.getElementById('cadastroForm');
+
+loginBtn.addEventListener('click', () => {
+    loginForm.classList.remove('hidden');
+    cadastroForm.classList.add('hidden');
+});
+
+cadastroBtn.addEventListener('click', () => {
+    cadastroForm.classList.remove('hidden');
+    loginForm.classList.add('hidden');
+});
+
+document.getElementById('loginSubmit').addEventListener('click', () => {
+    // Lógica para fazer login
+});
+
+document.getElementById('cadastroSubmit').addEventListener('click', () => {
+    // Lógica para cadastro
+});
